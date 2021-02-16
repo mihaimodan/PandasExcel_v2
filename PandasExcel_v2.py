96dc5af79a502aa81c911b02af0358b228407507
mihaimodan <78228595+mihaimodan@users.noreply.github.com>
2021-02-16 13:55:13 +0200

    Found & fixed an issue with duplicate output items.

--- PandasExcel_v2.py
+++ PandasExcel_v2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 #importing the excel content as a DataFrame.
-df = pd.read_excel(r"C:\Users\Mihai\Desktop\Programare\udemy\Ardit Sulce\The Python Mega Course - 10 real world applications\Exercises & other\Pandas\ItemsToCompare.xlsx", sheet_name = 0)
+df = pd.read_excel(r"C:\Users\Mihai\Desktop\Istoric_tranzactionare_EBGLDTL37.xlsx", sheet_name = 0)
 #Getting rid of all the possible "Unnamed" columns in the DataFrame.
 df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
 
@@ -10,13 +10,17 @@ df = df.loc[:, ~df.columns.str.contains("^Unnamed")]
 def lstCommonItems(datframe_input = df):
     lstCommon = []
     lstUncommon = []
+    lstCommon_v2 = []
     for k, v in df.iteritems():
         for k2, v2 in v.iteritems():
             if v2 not in lstUncommon:
                 lstUncommon.append(v2)
             else:
                 lstCommon.append(v2)
-    return lstCommon
+    for duplicate in lstCommon:#Found an issue with my original code. If it met the same item on let's say 3 separate columns, it will add that item 3 times in lstCommon.
+        if duplicate not in lstCommon_v2:
+            lstCommon_v2.append(duplicate)
+    return lstCommon_v2
 
 #Getting rid of the nan's from the resulted list.
 def delNan(lst = lstCommonItems):
@@ -26,4 +30,4 @@ def delNan(lst = lstCommonItems):
 #Converting the function to a data frame and assign it a column name.
 #Export to excel.
 df2 = pd.DataFrame(delNan(), columns = ["Common Items"])
-df2.to_excel(r"C:\Users\Mihai\Desktop\Programare\udemy\Ardit Sulce\The Python Mega Course - 10 real world applications\Exercises & other\Pandas\Output_v2.xlsx", columns = ["Common Items"])
+df2.to_excel(r"C:\Users\Mihai\Desktop\Output_v2.xlsx", columns = ["Common Items"])
